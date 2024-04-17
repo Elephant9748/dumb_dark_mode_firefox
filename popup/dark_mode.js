114c1e1b440f2775a02acbb114a82ac771547d91
@Elephant9748 <adytamasurya@protonmail.com>
2024-04-17 20:51:32 +0700

    add:dumb-dark-mode

---
+++ popup/dark_mode.js
@@ -0,0 +1,4 @@
+browser.tabs
+  .executeScript({ file: "run_dark_mode.js" });
+
+
---
+++ popup/run_dark_mode.js
@@ -0,0 +1,5 @@
+if (document.documentElement.style.filter == ""){
+        document.documentElement.style.filter = "invert(100%) hue-rotate(180deg)";
+} else {
+        document.documentElement.style.filter = "";
+}
